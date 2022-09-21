a2c8d95d1727f2def1329ffa00c023adfcaf194b
ValCrea <val.waldner@creatim.com>
2022-09-21 08:04:44 +0200

    Basic vueuse

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useMouse, useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <div :class="{ dark: isDark }" class="all">
+    <p @click="toggleDark()">Mouse at: {{ x }} {{ y }}</p>
+  </div>
+</template>
+
+<style scoped>
+.all {
+  width: 100%;
+  height: 100vh;
+}
+
+.dark {
+  background-color: black;
+  color: white;
+}
+</style>
